0d1a0ec33e975776386999ff5b54c1a9b90f3d3e
Aniketh Rao <anikethsrao@gmail.com>
2021-11-09 12:40:11 -0500

    ML Model Update
    Model Works but has very high loss

--- readWeatherCSV.py
+++ readWeatherCSV.py
@@ -2,6 +2,27 @@ import pandas as pd
 
 
 data = pd.read_csv('~/Documents/GitHub/Order-Assistant/WeatherData1.csv')
-data=data.to_json()
+data = data['data.weather'][1]
+
+print(data)
+def parse(start, end):
+    start_index = data.index(start)
+    end_index = data[start_index:].index(end)
+    final = data[start_index: start_index+end_index]
+    return final
+
+print(parse('avgTempC', ','))
+
+# print(data)
+# data.to_json()
+
+#data_json = data['data.weather'].to_json()
+
+# data_list = list(list(data['data.weather'])[0])
+
+#print(type(data_list))
+#print(data_list[0])
+"""
 for info in data['data.weather']:
-    print(info)
+    print(type(info))
+"""
